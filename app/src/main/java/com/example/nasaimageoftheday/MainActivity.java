b9b1f3e8bb135012f92bfc6d00a91f0891e54aed
nevena <nevena.miuchin@gmail.com>
2021-03-13 00:19:24 +0100

    add DatePicker

--- app/src/main/java/com/example/nasaimageoftheday/MainActivity.java
+++ app/src/main/java/com/example/nasaimageoftheday/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.nasaimageoftheday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MaterialButton btnDatePicker;
+    private int mYear, mMonth, mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the loaded button in the screen
+        btnDatePicker = findViewById(R.id.pick_date);
+        btnDatePicker.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    (view, year, monthOfYear, dayOfMonth) -> {
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
     }
 }

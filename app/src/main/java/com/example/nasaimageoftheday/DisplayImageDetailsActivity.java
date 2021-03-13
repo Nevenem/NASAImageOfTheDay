cf1525549b6421b7bf67d2d624fa730c10e92809
nevena <nevena.miuchin@gmail.com>
2021-03-13 02:01:09 +0100

    add AsynTask class MyHTTPRequest to send GET requests to NASA server

--- app/src/main/java/com/example/nasaimageoftheday/DisplayImageDetailsActivity.java
+++ app/src/main/java/com/example/nasaimageoftheday/DisplayImageDetailsActivity.java
@@ -2,24 +2,95 @@ package com.example.nasaimageoftheday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DisplayImageDetailsActivity extends AppCompatActivity {
 
     TextView imageUrlTextView;
+    TextView imageDescriptionTextView;
+
+    private String url;
+    private String imageDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_image_details);
 
-        String imageUrl = getIntent().getStringExtra("IMAGE_URL");
+        String url = getIntent().getStringExtra("IMAGE_URL");
 
-        // Get the imageUrl textview
+        // Get the imageUrl TextView
         imageUrlTextView = findViewById(R.id.image_url);
-        imageUrlTextView.setText(imageUrl);
+        imageUrlTextView.setText(url);
+
+        // Get the imageDescription TextView
+        imageDescriptionTextView = findViewById(R.id.image_description);
+
+        MyHTTPRequest req = new MyHTTPRequest();
+        req.execute(url);
+    }
+
+    private class MyHTTPRequest extends AsyncTask<String,Integer,String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            //create a URL object of what server to contact:
+            URL url = null;
+            try {
+                url = new URL(strings[0]);
+
+                // open the connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                // wait for data
+                InputStream response = urlConnection.getInputStream();
+
+                //JSON reading
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null ) {
+                    sb.append(line + "\n");
+                }
+                String result = sb.toString();
+
+                // convert to JSON
+                JSONObject imageDetailsJSON = new JSONObject(result);
+
+                // get the description of the image
+                imageDescription = imageDetailsJSON.getString("explanation");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            return "Done";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //set the imageDescriptionTextView text to show the image description
+            imageDescriptionTextView.setText(imageDescription);
+
+        }
     }
 }

3717f0266be6b2bc8d65513ad7cad7fc528ac9f7
nevena <nevena.miuchin@gmail.com>
2021-03-13 16:36:38 +0100

    add LoginActivity

--- app/src/main/java/com/example/nasaimageoftheday/LoginActivity.java
+++ app/src/main/java/com/example/nasaimageoftheday/LoginActivity.java
@@ -2,13 +2,18 @@ package com.example.nasaimageoftheday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView nameTextView;
     TextView passwordTextView;
+    MaterialButton LoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,13 @@ public class LoginActivity extends AppCompatActivity {
         nameTextView = findViewById(R.id.name);
         passwordTextView = findViewById(R.id.password);
 
+        // Get the Login button
+        MaterialButton loginButton = findViewById(R.id.login_button);
+
+
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
